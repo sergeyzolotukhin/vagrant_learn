16570b617bea64bee3ee723d9dc4c47c1223ca12
serhij.zolotukhin@gmail.com <serhij.zolotukhin@gmail.com>
2019-09-07 01:54:36 +0300

    vagrant

---
+++ src/main/java/ua/in/sz/vagrant/vagrant_learn/VagrantLearnApplication.java
@@ -0,0 +1,13 @@
+package ua.in.sz.vagrant.vagrant_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagrantLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VagrantLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/in/sz/vagrant/vagrant_learn/VagrantLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.in.sz.vagrant.vagrant_learn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VagrantLearnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
